f20a48f8d52daa241bce6a7c739f1a4820612441
Nathanael Memis <nathanaelmemis@gmail.com>
2024-04-16 14:37:56 +0800

    small change

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,19 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
-function initializeFirebase() {
+async function initializeFirebase() {
     let firebaseConfig = null;
 
     if (process.env.NODE_ENV === 'production') {
         firebaseConfig = process.env.FIREBASE_CONFIG;
     } else {
-        const module = import("./firebaseConfig");
+        const module = await import("./firebaseConfig");
         firebaseConfig = module.default;
     }
 
-    initializeApp(firebaseConfig);
+    return initializeApp(firebaseConfig);
 }
 
-const firebase = initializeFirebase();
+const firebase = await initializeFirebase();
 export const database = getDatabase(firebase);
 export const auth = getAuth(firebase)
